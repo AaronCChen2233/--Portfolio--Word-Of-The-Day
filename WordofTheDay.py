90758302019b3b28a2a88296976f7ce5bd760ce6
AaronCChen2233 <32030679+AaronCChen2233@users.noreply.github.com>
2019-10-05 09:10:18 -0700

    change naming rule

--- WordofTheDay.py
+++ WordofTheDay.py
@@ -6,6 +6,9 @@ from threading import Timer
 import webbrowser
 import pyttsx3
 import threading
+import numpy
+import sys
+from system_hotkey import SystemHotkey
 
 word = None
 nowword = 0
@@ -29,16 +32,19 @@ class BodyDragWindows(tk.Tk):
         self._offsetx = event.x
         self._offsety = event.y
 
-def GetWordOfTheDayInfo():
+
+def getWordOfTheDayInfo():
     global word
 
     url = "http://www.wordthink.com"
     html = req.get(url)
     root = ET.fromstring(html.text)
-    t = root.findall('{http://www.w3.org/2005/Atom}entry')
+    t = root.findall('h2','title')
     word = [obj.find('{http://www.w3.org/2005/Atom}title').text for obj in t]
-    wordoftheday.set(word[0])
-    setnowwordengword()
+    wordOfTheDay.set(word[0])
+    setNowWordEngWord()
+
+    """other resource"""
     # url = "http://word.eng.tw/"
     # html = req.get(url)
     # sp = BeautifulSoup(html.text, 'html.parser')
@@ -46,61 +52,75 @@ def GetWordOfTheDayInfo():
     # word = date[0].find_all('h3', {'class': 'post-title entry-title'})
     # wordoftheday.set(word[0].text)
 
-def nextword(event = None):
-    global nowword, wordoftheday, word
-    nowword+= 1
-    if nowword > len(word) -1:
-        nowword=len(word) -1
+def autoNextWord():
+    threading.Timer(600.0, autoNextWord).start()
+    nextWord()
 
-    wordoftheday.set(word[nowword])
-    setnowwordengword()
+def nextWord(event = None):
+    global nowword, wordOfTheDay, word
+    nowword += 1
+    if nowword > len(word) -1:
+        nowword = 0
+
+    wordOfTheDay.set(word[nowword])
+    setNowWordEngWord()
 
     # wordoftheday.set(word[nowword].text)
 
-def upword(event = None):
-    global nowword, wordoftheday, word
+def lastWord(event = None):
+    global nowword, wordOfTheDay, word
     nowword-=1
     if nowword <0:
         nowword =0
 
-    wordoftheday.set(word[nowword])
-    setnowwordengword()
+    wordOfTheDay.set(word[nowword])
+    setNowWordEngWord()
 
     # wordoftheday.set(word[nowword].text)
 
-def setnowwordengword():
-    global nowword, word,engword
-    # wordre = re.compile('[A-Za-z]+')
-    # engword = wordre.match(word[nowword]).group()
-
-    engword = word[nowword]
+def setNowWordEngWord():
+    global nowword, word, engword
+    wordre = re.compile('[A-Za-z]+')
+    engword = wordre.match(word[nowword]).group()
+    # engword = word[nowword]
 
 def tryGetWordOfTheDayInfo():
     try:
-        # GetWordOfTheDayInfo()
+        # getWordOfTheDayInfo()
 
-        GetWordOfTheDayInfoFromLocalFile()
+        getWordOfTheDayInfoFromLocalFile()
     except:
-        wordoftheday.set('網路問題稍後再試')
+        wordOfTheDay.set('Get Information Fail')
         t = Timer(30.0, tryGetWordOfTheDayInfo)
         t.start()
 
 
-def GetWordOfTheDayInfoFromLocalFile():
+def getWordOfTheDayInfoFromLocalFile():
     global word
     f = open("All Decks.txt", "rt", encoding="utf-8")
-    fl = f.readlines();
+    fl = f.readlines()
+    for index,w in enumerate(fl):
+        warry = w.split("</div> <div>")
+        w = warry[0]
+        cleanr = re.compile('<.*?>')
+        fl[index] = re.sub(cleanr, '', w)
+        fl[index] = fl[index].replace('\t', " ")
+        fl[index] = fl[index].replace('\n', "")
+
+
+    numpy.random.shuffle(fl)
     word = fl
-    wordoftheday.set(word[0])
-    setnowwordengword()
+    wordOfTheDay.set(word[0])
+    setNowWordEngWord()
 
 
 
-def SearchImage():
+def searchImage():
     global engword
     url="https://www.google.com.tw/search?q={}&source=lnms&tbm=isch&sa=X&biw=1280&bih=591&dpr=1.5".format(engword)
     openBrowser(url)
 
+"""spech library is not working now"""
 # def SpeechThread():
 #     global engword
 #     engine = pyttsx3.init()
@@ -111,13 +131,13 @@ def SearchImage():
 #     listenclipboard = threading.Thread(target=SpeechThread, args=())
 #     listenclipboard.start()
 
-def Speech():
-    global engword
-    engine = pyttsx3.init()
-    engine.say(engword)
-    engine.runAndWait()
+# def Speech():
+#     global engword
+#     engine = pyttsx3.init()
+#     engine.say(engword)
+#     engine.runAndWait()
 
-def Search():
+def search():
     global engword
     url="https://www.google.com.tw/search?q={}&sourceid=chrome&ie=UTF-8".format(engword)
     openBrowser(url)
@@ -128,27 +148,31 @@ def openBrowser(url):
     # webbrowser.open_new_tab(url)
 
 win = BodyDragWindows()
+
 win.configure(background='#0e285e')
 win.geometry("+997+574")
-wordoftheday=tk.StringVar()
-englishlabel = tk.Label(win, textvariable=wordoftheday, font=("微軟正黑體", 16), padx=20, pady=10, fg="#FFFFFF", bg='#0e285e')
+wordOfTheDay=tk.StringVar()
+englishlabel = tk.Label(win, textvariable=wordOfTheDay, font=("微軟正黑體", 16), padx=20, pady=10, fg="#FFFFFF", bg='#0e285e')
 englishlabel.grid(row=0, columnspan=5)
 
-upbtn = tk.Button(win, text="<", relief=FLAT, fg="#FFFFFF", bg='#0e285e', bd=0, width=5, command=upword)
+upbtn = tk.Button(win, text="<", relief=FLAT, fg="#FFFFFF", bg='#0e285e', bd=0, width=5, command=lastWord)
 upbtn.grid(row=1, column=0, sticky='w')
 
-nextbtn = tk.Button(win, text=">", relief=FLAT, fg="#FFFFFF", bg='#0e285e', bd=0, width=5, command=nextword)
+nextbtn = tk.Button(win, text=">", relief=FLAT, fg="#FFFFFF", bg='#0e285e', bd=0, width=5, command=nextWord)
 nextbtn.grid(row=1, column=4, sticky='e')
+
+appName = tk.Label(win, text="Word of the day", font=("微軟正黑體", 8), padx=20, pady=10, fg="#FFFFFF", bg='#0e285e')
+appName.grid(row=2, columnspan=5)
 
 #python去背圖顯示需再研究
 # searchimg = ImageTk.PhotoImage(file="image/search.png")
 # imageimg = ImageTk.PhotoImage(file="image/gallery.png")
 
-searchbtn = tk.Button(win,text='search', relief=FLAT, fg="#FFFFFF", bg='#0e285e', bd=0, width=5, command=Search)
+searchbtn = tk.Button(win, text='search', relief=FLAT, fg="#FFFFFF", bg='#0e285e', bd=0, width=5, command=search)
 searchbtn.grid(row=1, column=1)
 # searchbtn.config(image=searchimg)
 
-searchimagebtn = tk.Button(win, text='searchimage',relief=FLAT, fg="#FFFFFF", bg='#0e285e', bd=0, width=12, command=SearchImage)
+searchimagebtn = tk.Button(win, text='searchimage', relief=FLAT, fg="#FFFFFF", bg='#0e285e', bd=0, width=12, command=searchImage)
 searchimagebtn.grid(row=1, column=2)
 
 # speechbtn = tk.Button(win, text='speech',relief=FLAT, fg="#FFFFFF", bg='#0e285e', bd=0, width=5, command=Speech)
@@ -158,9 +182,14 @@ searchimagebtn.grid(row=1, column=2)
 win.lift()
 win.call('wm', 'attributes', '.', '-topmost', '1')
 win.overrideredirect(True)
-win.bind('<Left>', upword)
-win.bind('<Right >', nextword)
+win.bind('<Left>', lastWord)
+win.bind('<Right >', nextWord)
 tryGetWordOfTheDayInfo()
+
+# hk = SystemHotkey()
+# hk.register(('control'), callback=lambda:sys.exit(0))
+
+autoNextWord()
 
 win.mainloop()
 
